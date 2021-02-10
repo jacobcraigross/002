f37a75933cbc93720c2e187ad04b13700512a45f
Jacob Ross <survivaldawn@gmail.com>
2021-02-09 21:03:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-// Styles //
+var x = {
+  a: {
+    b: 2
+  }
+};
+// 2 objects are created. One is referenced by the other as one of its properties.
+// The other is referenced by virtue of being assigned to the 'x' variable.
+// Obviously, none can be garbage-collected.
 
-$("h1").css("fontFamily", "helvetica");
-$("img").css("width", "100px");
-$("body").css("background", "#f2f2f2");
-$("button").css({
-  fontFamily: "helvetica",
-  fontSize: "20px",
-  background: "#ff4040"
-});
+var y = x;      // The 'y' variable is the second thing that has a reference to the object.
 
-// Functionality //
+x = 1;          // Now, the object that was originally in 'x' has a unique reference
+                //   embodied by the 'y' variable.
 
-$("button").on("click", function(){
-  $(this).css("fontSize", "200px");
-  $(this).text("YOU BLEW IT.");
-});
+var z = y.a;    // Reference to 'a' property of the object.
+                //   This object now has 2 references: one as a property,
+                //   the other as the 'z' variable.
+
+y = 'mozilla';  // The object that was originally in 'x' has now zero
+                //   references to it. It can be garbage-collected.
+                //   However its 'a' property is still referenced by
+                //   the 'z' variable, so it cannot be freed.
+
+z = null;       // The 'a' property of the object originally in x
+                //   has zero references to it. It can be garbage collected.
